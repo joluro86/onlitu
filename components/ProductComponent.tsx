a97fbcb9746363cd5d1781c624f09a8080607da9
joluro86 <joluro86@hotmail.com>
2024-02-09 14:11:28 -0500

    cambios 1

--- components/ProductComponent.tsx
+++ components/ProductComponent.tsx
@@ -2,40 +2,49 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const ProductComponent = () => {
+// Definir un tipo para las props
+interface ProductComponentProps {
+    imageUrl: string;
+    imageAlt: string;
+    title: string;
+    description: string;
+    precio: string;
+    linkHref: string;
+    linkAs?: string; // Opcional, usando el signo de interrogación para indicar que esta prop es opcional
+}
+
+const ProductComponent: React.FC<ProductComponentProps> = ({ imageUrl, imageAlt, title, description, precio, linkHref, linkAs }) => {
     return (
-        <div className="max-w-[200px] min-w-[70] mt-4 bg-gray-100 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <Link href="#">
-                <Image
-                    className="rounded-t-lg cursor-pointer mx-auto"
-                    src="/static/images/protsan.png"
-                    alt=""
-                    width={70} // Estas dimensiones serán ignoradas si usas layout="responsive"
-                    height={70} // Estas dimensiones serán ignoradas si usas layout="responsive"
-                    
-                />
+        <div className="w-full my-4 sm:w-1/2 max-w-[200px] mx-auto mt-4 bg-gray-100 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <Link href={linkHref} as={linkAs || linkHref} passHref>
+                <div className="cursor-pointer">
+                    <Image
+                        className="rounded-t-lg"
+                        src={imageUrl}
+                        alt={imageAlt}
+                        width={200}
+                        height={200}
+                        priority={true}
+                    />
+                </div>
             </Link>
-            <div className="border-solid px-2 pb-4 bg-gray-50">
-                <Link href="#">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white cursor-pointer">PROTSAN</h5>
+            <div className="px-2 pb-4 bg-gray-50">
+                <Link href={linkHref} as={linkAs || linkHref} passHref>
+                    <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-700 dark:text-white cursor-pointer">{title}</h5>
                 </Link>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Medicamento natural para la inflamación de la próstata.</p>
-                <Link href="#">
+                <p className="mb-3 font-normal text-sm text-gray-700 dark:text-gray-400">{description}</p>
+                <p className="mb-3 font-normal text-sm text-gray-700 dark:text-gray-400">{precio}</p>
+                <Link href={linkHref} as={linkAs || linkHref} passHref>
                     <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer">
-                        Mas información
-                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                        Saber más
+                        <svg className="ml-2 w-3.5 h-3.5" aria-hidden="true" fill="none" viewBox="0 0 14 10" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1 5h12m0 0L9 1m4 4L9 9" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"/>
                         </svg>
                     </div>
                 </Link>
             </div>
         </div>
-
-
-
-
     );
 }
 
 export { ProductComponent };
-
--- pages/productos.tsx
+++ pages/productos.tsx
@@ -3,18 +3,64 @@ import React from 'react'
 
 const productos = () => {
   return (
-    <div className='mx-auto max-w-[1200px] m-8'>
-      <h1 className='mb-4 text-lg text-gray-900 dark:text-white'>Productos</h1>
-      <div className="flex flex-wrap justify-center gap-4 bg-gray-900">
-        <ProductComponent />
-        <ProductComponent />
-        <ProductComponent />
-        <ProductComponent />
-        <ProductComponent />
-        <ProductComponent />
-        <ProductComponent />
-        <ProductComponent />
-      </div>
+    <div className='flex flex-wrap justify-center md:justify-start gap-4 m-8 max-w-[1000px] mx-auto bg-red-900'>
+      <ProductComponent
+        imageUrl="/static/images/protsan.png"
+        imageAlt="Protsan"
+        title="Protsan"
+        description="Medicamento natural para la inflamación de la próstata."
+        precio= 'Precio unidad: 79,900'
+        linkHref="/producto/protsan"
+      />
+      <ProductComponent
+        imageUrl="/static/images/protsan.png"
+        imageAlt="Protsan"
+        title="Protsan"
+        description="Medicamento natural para la inflamación de la próstata."
+        precio= 'Precio unidad: 79,900'
+        linkHref="/producto/protsan"
+      />
+      <ProductComponent
+        imageUrl="/static/images/protsan.png"
+        imageAlt="Protsan"
+        title="Protsan"
+        description="Medicamento natural para la inflamación de la próstata."
+        precio= 'Precio unidad: 79,900'
+        linkHref="/producto/protsan"
+      />
+      <ProductComponent
+        imageUrl="/static/images/protsan.png"
+        imageAlt="Protsan"
+        title="Protsan"
+        description="Medicamento natural para la inflamación de la próstata."
+        precio= 'Precio unidad: 79,900'
+        linkHref="/producto/protsan"
+      />
+      <ProductComponent
+        imageUrl="/static/images/protsan.png"
+        imageAlt="Protsan"
+        title="Protsan"
+        description="Medicamento natural para la inflamación de la próstata."
+        precio= 'Precio unidad: 79,900'
+        linkHref="/producto/protsan"
+      />
+      <ProductComponent
+        imageUrl="/static/images/protsan.png"
+        imageAlt="Protsan"
+        title="Protsan"
+        description="Medicamento natural para la inflamación de la próstata."
+        precio= 'Precio unidad: 79,900'
+        linkHref="/producto/protsan"
+      />
+      <ProductComponent
+        imageUrl="/static/images/protsan.png"
+        imageAlt="Protsan"
+        title="Protsan"
+        description="Medicamento natural para la inflamación de la próstata."
+        precio= 'Precio unidad: 79,900'
+        linkHref="/producto/protsan"
+      />
+      
     </div>
   )
 }
